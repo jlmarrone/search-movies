5ce6936558cbc607411f4e8943a697d2830bde95
José Luis Marrone <jlmarrone@gmail.com>
2020-01-09 08:28:44 -0300

    Improvements to show movies list and details

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
 import React, { Component } from 'react';
-import { Title } from './components/Title'
-import { SearchForm } from './components/SearchForm'
+import { Switch, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
+import { NotFound } from './pages/NotFound'
+
 import './App.css';
 import 'bulma/css/bulma.css'
 
 class App extends Component {
-  state = { usedSearch: false, results: [] }
-
-  _handleResults = (results) => {
-    this.setState({ results, usedSearch: true })
-  }
-
-  _renderResults () {
-    return this.state.results.length === 0
-      ? <p>Sorry! 😞 Results not found!</p>
-      : <p>Results Found!</p>
-  }
-
   render() {
     return (
       <div className="App">
-        <Title>
-          Search Movies
-      </Title>
-        <div className="search-form-wrapper">
-          <SearchForm onResults={this._handleResults}></SearchForm>
-        </div>
-        {this.state.usedSearch
-          ? this._renderResults()
-          : <small>Use the form to search a movie</small>
-        }
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/detail/:movieId' component={Detail} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
-    )
+    );
   }
 }
 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,47 +3,45 @@ import React, { Component } from 'react'
 const API_KEY = '74107567'
 
 export class SearchForm extends Component {
-    state = {
-        inputMovie: ''
-    }
+  state = {
+    inputMovie: ''
+  }
 
-    _handleChange = (e) => {
-        this.setState({inputMovie: e.target.value})
-    }
+  _handleChange = (e) => {
+    this.setState({ inputMovie: e.target.value })
+  }
 
-    _handleSubmit = (e) => {
-        e.preventDefault()
+  _handleSubmit = (e) => {
+    e.preventDefault()
+    const { inputMovie } = this.state
 
-        const { inputMovie } = this.state
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+      .then(res => res.json())
+      .then(results => {
+        const { Search = [], totalResults = "0" } = results
+        console.log({ Search, totalResults })
+        this.props.onResults(Search)
+      })
+  }
 
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
-            .then(res => res.json())
-            .then(results => {
-                const { Search = [], totalResults = "0" } = results
-                console.log({ Search, totalResults })
-                this.props.onResults(Search)
-            })
-    }
-
-    render() {
-        return (
-            <form onSubmit={this._handleSubmit}>
-                <div className="field has-addons">
-                    <div className="control">
-                        <input
-                            className="input"
-                            onChange={this._handleChange}
-                            type="text"
-                            placeholder="Find a repository">
-                        </input>
-                    </div>
-                    <div className="control">
-                        <a className="button is-info">
-                            Movie to Search...
-                        </a>
-                    </div>
-                </div>
-            </form>
-        )
-    }
+  render () {
+    return (
+      <form onSubmit={this._handleSubmit}>
+        <div className="field has-addons">
+          <div className="control">
+            <input
+              className="input"
+              onChange={this._handleChange}
+              type="text"
+              placeholder="Movie to search..." />
+          </div>
+          <div className="control">
+            <button className="button is-info">
+              Search
+            </button>
+          </div>
+        </div>
+      </form>
+    )
+  }
 }
